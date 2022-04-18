5e9dcaa53c12342239244e5d000abbf1fae88801
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-04-18 11:13:26 -0400

    pushing lesson

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,25 @@
+const middleware = require('../middleware')
+
 const { User } = require('../models')
 const Login = async (req, res) => {
   try {
+    const user = await User.findOne({
+      where: { email: req.body.email },
+      raw: true
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(user.passwordDigest, req.body.password))
+    ) {
+      // Next code goes here
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
   } catch (error) {
     throw error
   }
@@ -8,6 +27,31 @@ const Login = async (req, res) => {
 
 const Register = async (req, res) => {
   try {
+    const { email, password, name } = req.body
+    let passwordDigest = await middleware.hashPassword(password)
+    const user = await User.create({ email, passwordDigest, name })
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdatePassword = async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } })
+    if (
+      user &&
+      (await middleware.comparePassword(
+        user.dataValues.passwordDigest,
+        req.body.oldPassword
+      ))
+    ) {
+      let passwordDigest = await middleware.hashPassword(req.body.newPassword)
+
+      await user.update({ passwordDigest })
+      return res.send({ status: 'Success', msg: 'Password Updated' })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Invalid Credentials' })
   } catch (error) {
     throw error
   }
@@ -15,5 +59,6 @@ const Register = async (req, res) => {
 
 module.exports = {
   Login,
-  Register
+  Register,
+  UpdatePassword
 }
